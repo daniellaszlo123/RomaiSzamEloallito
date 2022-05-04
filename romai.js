8216247e29636bc3d15109f94b6dacf6e9135ec6
daniellaszlo123 <daniellaszlo002@gmail.com>
2022-05-04 15:22:13 +0200

    39-ig jó

--- romai.js
+++ romai.js
@@ -1,12 +1,53 @@
 function romaiSzam(n) {
-    //let szamjegyek=;
     let romai="";
     if (n>0 && n<=3) {
-        for (let i = 0; i < n; i++) {
-            romai+="I";
-        }
-    }else if (n===4) {
-        romai+="IV";
+        romai+=iBetuk(n);
     }
+    else if (n>=4 && n<=9) {
+        romai+=negyEs9Kozott(n);
+    }
+    else if (n>=10 && n<=39) {
+        console.log("bekert szam: "+n);
+        tizesDb=n/10;
+        tizesDb=Math.floor(tizesDb);
+        console.log("kerekitett szam: "+tizesDb);
+        for (let i = 0; i < tizesDb; i++) {
+            romai+="X";
+        }
+        n=n-tizesDb*10;
+        console.log("tizes nelkuli "+n);
+        if (n>0 && n<=3) {
+            romai+=iBetuk(n);
+        }else if(n>=4 && n<=9){
+            romai+=negyEs9Kozott(n);
+        }
+    }
+    
     return romai;
+}
+
+function iBetuk(n) {
+    let romaiVisszateres="";
+    for (let i = 0; i < n; i++) {
+        romaiVisszateres+="I";
+    }
+    return romaiVisszateres;
+}
+
+function negyEs9Kozott(n) {
+    let romaiVisszateres="";
+    if (n===4) {
+        romaiVisszateres+="IV";
+    }
+    else if (n===9) {
+        romaiVisszateres+="IX";
+    }
+    else{
+        romaiVisszateres+="V";
+        n-=5;
+        if (n>0 && n<=3) {
+            romaiVisszateres+=iBetuk(n);
+        }
+    }
+    return romaiVisszateres;
 }
--- teszt/romaiTeszt.js
+++ teszt/romaiTeszt.js
@@ -7,18 +7,59 @@ QUnit.module('romaiSzam', function() {
     });    
 
     QUnit.test('n=1', function(assert) {
-      assert.equal(romaiSzam(1),"I");
+      assert.equal(iBetuk(1),"I");
     });
 
     QUnit.test('n=2', function(assert) {
-      assert.equal(romaiSzam(2),"II");
+      assert.equal(iBetuk(2),"II");
     });
 
     QUnit.test('n=3', function(assert) {
-      assert.equal(romaiSzam(3),"III");
+      assert.equal(iBetuk(3),"III");
     });
 
     QUnit.test('n=4', function(assert) {
-      assert.equal(romaiSzam(4),"IV");
+      assert.equal(negyEs9Kozott(4),"IV");
     });
+
+    QUnit.test('n=5', function(assert) {
+      assert.equal(negyEs9Kozott(5),"V");
+    });
+
+    QUnit.test('n=6', function(assert) {
+      assert.equal(negyEs9Kozott(6),"VI");
+    });
+
+    QUnit.test('n=7', function(assert) {
+      assert.equal(negyEs9Kozott(7),"VII");
+    });
+
+    QUnit.test('n=8', function(assert) {
+      assert.equal(negyEs9Kozott(8),"VIII");
+    });
+
+    QUnit.test('n=9', function(assert) {
+      assert.equal(negyEs9Kozott(9),"IX");
+    });
+
+    QUnit.test('n=10', function(assert) {
+      assert.equal(romaiSzam(10),"X");
+    });
+
+    QUnit.test('n=11', function(assert) {
+      assert.equal(romaiSzam(11),"XI");
+    });
+    
+    QUnit.test('n=27', function(assert) {
+      assert.equal(romaiSzam(27),"XXVII");
+    });
+    
+    QUnit.test('n=39', function(assert) {
+      assert.equal(romaiSzam(39),"XXXIX");
+    });
+    
+    QUnit.test('n=40', function(assert) {
+      assert.equal(romaiSzam(40),"XL");
+    });
+
   });
